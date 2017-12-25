2614115e9b9f624a0512ac087e00faf7cbaf391b
viviniko <viviniko.shop@gmail.com>
2017-12-25 13:29:48 +0800

    -

--- src/Repositories/Chapter/ChapterRepository.php
+++ src/Repositories/Chapter/ChapterRepository.php
@@ -4,5 +4,14 @@ namespace Viviniko\Book\Repositories\Chapter;
 
 interface ChapterRepository
 {
-
+    /**
+     * Paginate books.
+     *
+     * @param $bookId
+     * @param $pageSize
+     * @param string $searchName
+     * @param null $search
+     * @return mixed
+     */
+    public function paginate($bookId, $pageSize, $searchName = 'search', $search = null);
 }
