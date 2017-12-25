93f9122fb435e3c900c92e018eb1927e50b9e466
viviniko <viviniko.shop@gmail.com>
2017-12-25 16:44:45 +0800

    attributes

--- src/Repositories/Book/EloquentBook.php
+++ src/Repositories/Book/EloquentBook.php
@@ -2,6 +2,7 @@
 
 namespace Viviniko\Book\Repositories\Book;
 
+use Illuminate\Support\Facades\DB;
 use Viviniko\Repository\SimpleRepository;
 
 class EloquentBook extends SimpleRepository implements BookRepository
@@ -23,5 +24,41 @@ class EloquentBook extends SimpleRepository implements BookRepository
         }
 
         return $items;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function create(array $data)
+    {
+        $book = null;
+
+        DB::transaction(function () use ($data, &$book) {
+            $book = parent::create($data);
+
+            if (!empty($data['attributes'])) {
+                $book->attributes()->sync($data['attributes']);
+            }
+        });
+
+        return $book;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function update($id, array $data)
+    {
+        $book = null;
+
+        DB::transaction(function () use ($id, $data, &$product) {
+            $book = parent::update($id, $data);
+
+            if (!empty($data['attributes'])) {
+                $book->attributes()->sync($data['attributes']);
+            }
+        });
+
+        return $book;
     }
 }
