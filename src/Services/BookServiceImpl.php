dde0b12524742d50d9680b1f7bc5c75a713c6f1a
viviniko <viviniko.shop@gmail.com>
2017-12-25 16:57:58 +0800

    attribute

--- src/Services/BookServiceImpl.php
+++ src/Services/BookServiceImpl.php
@@ -38,10 +38,6 @@ class BookServiceImpl implements BookService
         if ($latestChapter->number != $book->latest_chapter) {
             $data['latest_chapter'] = $latestChapter->number;
         }
-        $chapterCount = $this->chapterRepository->countChapterByBookId($bookId);
-        if ($chapterCount != $book->chapter_count) {
-            $data['chapter_count'] = $chapterCount;
-        }
 
         if (!empty($data))
             $this->bookRepository->update($bookId, $data);
