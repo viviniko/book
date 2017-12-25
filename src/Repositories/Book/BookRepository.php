e34ee679449f3ab2491659b3cb7fd2d18683a16c
viviniko <viviniko.shop@gmail.com>
2017-12-25 10:32:13 +0800

    book repository

--- src/Repositories/Book/BookRepository.php
+++ src/Repositories/Book/BookRepository.php
@@ -4,5 +4,52 @@ namespace Viviniko\Book\Repositories\Book;
 
 interface BookRepository
 {
+    /**
+     * Paginate books.
+     *
+     * @param $pageSize
+     * @param string $searchName
+     * @param null $search
+     * @return mixed
+     */
+    public function paginate($pageSize, $searchName = 'search', $search = null);
+
+    /**
+     * Find data by id
+     *
+     * @param       $id
+     * @param       $columns
+     *
+     * @return mixed
+     */
+    public function find($id, $columns = ['*']);
+
+    /**
+     * Save a new entity in repository
+     *
+     * @param array $data
+     *
+     * @return mixed
+     */
+    public function create(array $data);
+
+    /**
+     * Update a entity in repository by id
+     *
+     * @param       $id
+     * @param array $data
+     *
+     * @return mixed
+     */
+    public function update($id, array $data);
+
+    /**
+     * Delete a entity in repository by id
+     *
+     * @param $id
+     *
+     * @return int
+     */
+    public function delete($id);
 
 }
--- src/Repositories/Book/EloquentBook.php
+++ src/Repositories/Book/EloquentBook.php
@@ -8,4 +8,20 @@ class EloquentBook extends SimpleRepository implements BookRepository
 {
     protected $modelConfigKey = 'book.book';
 
+    /**
+     * {@inheritdoc}
+     */
+    public function paginate($pageSize, $searchName = 'search', $search = null)
+    {
+        if (!$search) {
+            $search = request()->get($searchName);
+        }
+        $search = is_array($search) ? $search : [];
+        $items = $this->search($search)->paginate($pageSize);
+        if (!empty($search)) {
+            $items->appends([$searchName => $search]);
+        }
+
+        return $items;
+    }
 }
